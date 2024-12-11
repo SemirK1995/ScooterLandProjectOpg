c5a452c3530cb0283e60f9981d5c08d71f0214c2
Gambajuice <Rasmus_oliver@hotmail.com>
2024-12-11 02:50:36 +0100

    Scooterting

---
+++ ScooterLandProjectOpg/Client/wwwroot/js/scooter-animation.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const scooter = document.querySelector('.scooter');
+    scooter.style.animation = 'none';
+    setTimeout(() => {
+        scooter.style.animation = '';
+    }, 10);
+});
